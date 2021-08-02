c90e5083f29e7d61ccc8d7251358ef6194f00a54
OmaxrAli <omar.abdelmajeed17@gmail.com>
2021-08-02 23:33:36 +0300

    Adding about me

---
+++ js.js
@@ -0,0 +1,58 @@
+
+
+var name = prompt("input your name pleas")
+
+
+
+var questionA = prompt("Do you want to know my age Y/N")
+.toUpperCase(questionA)
+
+if(questionA=='Y'){
+    alert("21")
+}
+else if(questionA=='N'){
+    alert("Ok")
+}
+
+var questionB = prompt(" do you want to know my passion Y/N")
+.toUpperCase(questionB)
+if(questionB=='Y'){
+    alert("I am so tired but i really want to be so good at this ")
+}
+else if(questionB=='N'){
+    alert("Ok")
+}
+
+var questionC = prompt("Do you want to know where do i live Y/N")
+.toUpperCase(questionC)
+
+if(questionC == 'Y'){
+    alert("Jordan")
+}
+else if(questionC =='N'){
+    alert("Ok")
+}
+
+var questionD = prompt("Do you want to know my fun fact Y/N")
+.toUpperCase(questionD)
+
+if(questionD=='Y'){
+    alert("I can be very good at something for real but i feel that im not in the mood for big things")
+}
+else if(questionD=='N'){
+    alert("Ok")
+}
+
+var questionE = prompt("Do you want to know my secret Y/N")
+.toUpperCase(questionE)
+
+if(questionE =='Y'){
+    alert("I dont smoke")
+}
+else if(questionE =='N'){
+    alert("Ok")
+}
+
+
+
+document.write(" Welcom "+name )
