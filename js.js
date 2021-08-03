920c5de20bbd1d5631fed1d36fd79caf3a36980a
OmaxrAli <omar.abdelmajeed17@gmail.com>
2021-08-03 17:20:49 +0300

    Adding two questions

--- js.js
+++ js.js
@@ -2,57 +2,113 @@
 
 var name = prompt("input your name pleas")
 
+alert(" Welcom "+name )
+
+let Score =0;
 
 
-var questionA = prompt("Do you want to know my age Y/N")
+
+
+var questionA = prompt("Is my age 22? Y/N")
 .toUpperCase(questionA)
 
 if(questionA=='Y'){
-    alert("21")
+    alert("no you are worng")
 }
 else if(questionA=='N'){
-    alert("Ok")
+    alert("Yes you are right")
+    Score++;
 }
 
-var questionB = prompt(" do you want to know my passion Y/N")
+var questionB = prompt("Is my passion \"I am so tired but i really want to be so good at this\" Y/N")
 .toUpperCase(questionB)
 if(questionB=='Y'){
-    alert("I am so tired but i really want to be so good at this ")
+    alert("Yes you are right")
+    Score++;
 }
 else if(questionB=='N'){
-    alert("Ok")
+    alert("no you are worng")
 }
 
-var questionC = prompt("Do you want to know where do i live Y/N")
+var questionC = prompt("do i live in Jordan Y/N")
 .toUpperCase(questionC)
 
 if(questionC == 'Y'){
-    alert("Jordan")
+    alert("Yes you are right")
+    Score++;
 }
 else if(questionC =='N'){
-    alert("Ok")
+    alert("no you are worng")
 }
 
-var questionD = prompt("Do you want to know my fun fact Y/N")
+var questionD = prompt("is fun fact \"I can be very good at something for real but i feel that im not in the mood for big things\" Y/N")
 .toUpperCase(questionD)
 
 if(questionD=='Y'){
-    alert("I can be very good at something for real but i feel that im not in the mood for big things")
+    alert("Yes you are right")
+    Score++;
 }
 else if(questionD=='N'){
-    alert("Ok")
+    alert("no you are worng")
 }
 
-var questionE = prompt("Do you want to know my secret Y/N")
+var questionE = prompt("my secret is \"I dont smoke\" Y/N")
 .toUpperCase(questionE)
 
 if(questionE =='Y'){
-    alert("I dont smoke")
+    alert("no you are worng")
 }
 else if(questionE =='N'){
-    alert("Ok")
+    alert("Yes you are right")
+    Score++;
 }
 
+for(var i=0; i < 4; i++ ){
+
+    var questionF = prompt("pleas enter a number between 1 and 23")
+    if(questionF== 21){
+        alert("Correct")
+        Score++;
+        break;
+    }
+    else if(questionF > 21 && questionF <=23 ){
+        alert("too high")
+    }
+        else if(questionF < 21 && questionF>= 1){
+            alert("too low")
+        }
+        else{
+            alert("follow the rules")
+        }
+}
+
+    let arr=["BMW", "Tesla", "Cyber Dragons", "apple", "juce", "mug","Asus", "Orang internet"]
+    alert(arr)
+    alert("some of these are cars tell only one of them to win a point")
+    var questionG = prompt("type here please")
 
 
-document.write(" Welcom "+name )
+    var ss=0
+    for(i=0 ; i< 6; i++){
+        for(j=0 ; j < arr.length-1; j++){
+
+            if(questionG == arr[0] || questionG== arr[1]){
+                alert("you are right")
+                Score++;
+                ss =1;
+                break;
+            }
+            else{
+                alert("you are wrong")
+                break
+            }
+        }
+        if(ss){
+        break
+    }
+    questionG=prompt("try again")
+    }
+
+ alert("your score is "+ Score)
+
+
